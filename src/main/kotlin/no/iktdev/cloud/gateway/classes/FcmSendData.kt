b2dd72626f3c0a6a9e530269ce09de59bdb3f4e9
Brage Skjonborg <brage.skjonborg@hotmail.no>
2022-05-22 03:41:29 +0200

    Corrected typo

--- src/main/kotlin/no/iktdev/cloud/gateway/classes/FcmSendData.kt
+++ src/main/kotlin/no/iktdev/cloud/gateway/classes/FcmSendData.kt
@@ -3,7 +3,7 @@ package no.iktdev.cloud.gateway.classes
 /**
  * @param appPackage Package of app calling
  * @param fcmSenderToken Device Sender Token
- * @param fmcTargets List of FCM device tokens
+ * @param fcmTargets List of FCM device tokens
  * @param payload Json String encoded on sender device
  */
-data class FcmSendData(val appPackage: String, val fcmSenderToken: String, val fmcTargets: List<String>, val payload: Map<String, String>)
+data class FcmSendData(val appPackage: String, val fcmSenderToken: String, val fcmTargets: List<String>, val payload: Map<String, String>)
