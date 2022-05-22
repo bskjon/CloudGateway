0c757d268d885c5102af910578ad682fa6768071
Brage Skjonborg <brage.skjonborg@hotmail.no>
2022-05-22 04:47:16 +0200

    Added heartbeat

---
+++ src/main/kotlin/no/iktdev/cloud/gateway/controller/Heartbeat.kt
@@ -0,0 +1,33 @@
+package no.iktdev.cloud.gateway.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import javax.servlet.http.HttpServletResponse
+
+@RestController
+class Heartbeat {
+    private fun heartbeat(): Heartbeat
+    {
+        return Heartbeat(true, System.currentTimeMillis() / 1000L)
+    }
+
+    @GetMapping("/heartbeat")
+    fun heartbeatPath(): Heartbeat
+    {
+        return heartbeat()
+    }
+
+    @GetMapping("/")
+    fun defaultPath(): Heartbeat
+    {
+        return heartbeat()
+    }
+
+    @GetMapping("/swagger")
+    fun swaggerRedirect(response: HttpServletResponse) {
+        response.setHeader("Location", "/swagger-ui.html")
+        response.status = 302
+    }
+
+    data class Heartbeat(val status: Boolean, val time: Long)
+}
